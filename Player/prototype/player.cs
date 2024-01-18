3df4984456fb29caf637ef2322761d01a3c1285f
Alexander <alex030t@stud.kea.dk>
2024-01-18 20:28:55 +0100

    Added other 3 limbs to the player character. Still WIP

--- Player/prototype/player.cs
+++ Player/prototype/player.cs
@@ -5,10 +5,17 @@ public partial class player : Node2D
 {
 
 	private RigidBody2D LeftArm;
+	private RigidBody2D RightArm;
+	private RigidBody2D LeftLeg;
+	private RigidBody2D RightLeg;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		LeftArm = GetNode<RigidBody2D>("UpperArm/LowerArm");
+		LeftArm = GetNode<RigidBody2D>("UpperArmLeft/LowerArmLeft");
+		RightArm = GetNode<RigidBody2D>("UpperArmRight/LowerArmRight");
+		LeftLeg = GetNode<RigidBody2D>("UpperLegLeft/LowerLegLeft");
+		RightLeg = GetNode<RigidBody2D>("UpperLegRight/LowerLegRight");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,6 +24,18 @@ public partial class player : Node2D
 		if(Input.IsActionPressed("Q"))
 		{
 			ApplyForceToLimb(LeftArm, 50);
+		}
+		else if(Input.IsActionPressed("E"))
+		{
+			ApplyForceToLimb(RightArm, 50);
+		}
+		else if(Input.IsActionPressed("W"))
+		{
+			ApplyForceToLimb(LeftLeg, 50);
+		}
+		else if(Input.IsActionPressed("R"))
+		{
+			ApplyForceToLimb(RightLeg, 50);
 		}
 	}
 
