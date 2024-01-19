3da40b5cea0d50f914594ba68ad1ce7b67341667
bupledox <bupledox@gmail.com>
2024-01-19 20:18:50 +0000

    rthsrth

---
+++ Components/Limb/Limb.cs
@@ -0,0 +1,98 @@
+using Godot;
+using System;
+
+[Tool]
+public partial class Limb : Node2D
+{
+	[Export]
+	float length1;
+	[Export]
+	float length2;
+
+	[Export]
+	bool reverseKnee=false;
+
+	[Export]
+	Node2D targetNode,shoulderMinNode;
+
+	Node2D elbowNode,endNode,sholderNode;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		elbowNode = GetNode<Node2D>("Elbow");
+		endNode = GetNode<Node2D>("End");
+		shoulderMinNode = GetNode<Node2D>("MinShoulder");
+		sholderNode = GetNode<Node2D>("Shoulder");
+	}
+
+	private float cosineRuleForAngle(float a, float b, float c){
+
+		return Mathf.Acos((a*a+b*b-c*c)/(2*a*b));
+		
+	}
+	private Vector2 getShoulderPos(Vector2 start,Vector2 target,Vector2 shoulderMin){
+		var delta = target-start;
+		var shoulderThreshold = length1/2;
+		
+		var stoStartLerp = start+((delta*0.05f).Normalized()*(Math.Min((delta*0.1f).Length(),shoulderThreshold)));
+		//var stoStartLerp = start.add(target.sub(start).normalised(-0.2).rotate(Math.PI/4));
+		//var stoSMinLerp = shoulderMin.add(target.sub(shoulderMin).times(0.1));
+		//drawer.draw([shoulderMin,stoSMinLerp])
+		var lerpAmount = 1-(Mathf.Min(shoulderThreshold,(start-target).Length())/shoulderThreshold);
+		return stoStartLerp.Lerp(shoulderMin,lerpAmount);
+	}
+	private void updateJointPositions(){
+
+		if(targetNode==null||elbowNode==null||endNode==null||shoulderMinNode==null||sholderNode==null){
+			return;
+		}
+		
+		var fromNode = this;
+		var target = targetNode.Position;
+		var start = getShoulderPos(fromNode.Position,target,shoulderMinNode.Position);
+
+		sholderNode.Position = start;
+
+		var targetDelta = target-start;
+
+
+		Vector2 elbowPos = Vector2.Zero;
+
+		if(length1+length2<targetDelta.Length()){
+			elbowPos = targetDelta.Normalized()*length1+start;
+			//out of range so straight arm
+		}
+		else{
+			
+			var minLength = Math.Abs(length1-length2);
+
+			if(minLength>targetDelta.Length()){
+				//too close so move the target.
+				targetDelta = targetDelta.Normalized()*(minLength+0.00001f);
+			}
+			
+			var angleFromDeltaToElbow = cosineRuleForAngle(length1,targetDelta.Length(),length2)*(reverseKnee?-1:1);
+			
+			var arm1Vec = targetDelta.Rotated(angleFromDeltaToElbow).Normalized()*length1;
+
+			elbowPos = start+arm1Vec;
+				
+			
+		}
+
+
+		var elbowToTarget = target-elbowPos;
+
+		var endPos = (elbowToTarget.Normalized()*length2)+elbowPos;
+
+		endNode.Position = endPos;
+		elbowNode.Position = elbowPos;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		updateJointPositions();
+	}
+}
---
+++ Components/Limb/SpriteBetweenPoints.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+[Tool]
+public partial class SpriteBetweenPoints : Node2D
+{
+	[Export]
+	Node2D nodeFrom;
+	[Export]
+	Node2D nodeTo;
+	
+	[Export]
+	float rescale = 1;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		//ogWidth = GetNode<CollisionShape2D>("CollisionShape2D").Shape.GetRect().Size.X;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if(nodeFrom==null || nodeTo==null){return;}
+		this.Position = (nodeFrom.Position + nodeTo.Position)/2;
+		
+		//assume the width is 1;
+		var deltaP = nodeFrom.Position - nodeTo.Position;
+
+		var scale = (deltaP).Length()*rescale;
+
+		this.Scale = new Vector2(scale,scale); 
+
+		this.Rotation = -deltaP.AngleTo(Vector2.Right);
+	}
+}
