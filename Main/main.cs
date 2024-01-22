0ca00d41eb1be3f9772eb3ca76b066b0812b9cc0
bupledox <bupledox@gmail.com>
2024-01-22 22:32:33 +0000

    Some more demo scenes and a dev level system ish

--- Main/main.cs
+++ Main/main.cs
@@ -1,21 +1,52 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class main : Node2D
 {
 	[Export]
 	PackedScene[] Levels;
+	
+	[Export]
+	Label titleLabel;
 
+	Node loadedScene;
+	int currentSceneIndex = 0;
+	List<Node> loadedLevels;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var ins = Levels[0].Instantiate();
+		LoadScene(0);
+	}
+
+	private void LoadScene(int index){
+
+		index = index%(Levels.Count()-1);//loop
+
+
+		if(loadedScene!=null){
+			loadedScene.QueueFree();
+		}
+
+		var ins = Levels[index].Instantiate();
 		AddChild(ins);
+
+		titleLabel.Text = "Title: "+Levels[index].ResourcePath;
+
+		loadedScene = ins;
+		currentSceneIndex = index;
+
 	}
 
     public override void _Input(InputEvent @event)
     {
-	
+		if(Input.IsActionJustPressed("DevNextLevel")){
+			LoadScene(currentSceneIndex+1);
+		}
+		if(Input.IsActionJustPressed("DevPrevLevel")){
+			LoadScene(currentSceneIndex-1);
+		}
 
         base._Input(@event);
     }
